830c4c26c000aa8bd83b23a00d3dee949fd364c3
edward mbeleki <embeleki@c4r4s1.wethinkcode.co.za>
2020-01-11 10:36:23 +0200

    done

---
+++ h.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+
+int fn(int i);
+  int ft_recursive_factorial(int nb);
+{
+ if (i <= 9)
+   {
+     i += 1;
+     write(1 ,"d", 1);
+  fn(i);
+  write(1,"f",1);
+  return 0;
+   }
+ else
+ {
+  return 0;
+ }
+}
+
+int main()
+{
+  fn(0);
+  return 0;
+}
